e8e9fcde72dba316773a33b10e5a4e3f9c8b65c3
Aditya Bhat <adityadb24@gmail.com>
2022-05-06 10:03:39 -0700

    added argparse to parse CLI arguments

--- raspberry-pi/record_video_and_send_API_request.py
+++ raspberry-pi/record_video_and_send_API_request.py
@@ -2,17 +2,18 @@
 import picamera
 from multiprocessing import Process
 import base64
-import json                    
+import json
 import time
 import requests
+import argparse
 
-BUCKET = "s3://ccinputvideobucket/"
 
 # function to send request to API gateway with the video file
 def send_API_request(*args, **kwargs):
     file_h264 = kwargs['file_h264']
     VIDEO_NAME = kwargs['VIDEO_NAME']
-    api = 'https://w97zrvuvtb.execute-api.us-east-1.amazonaws.com/dev/' +VIDEO_NAME
+    BASE_URL = kwargs['BASE_URL']
+    api = BASE_URL +VIDEO_NAME
     # print("API:", api)
 
     # read the video file
@@ -36,19 +37,19 @@ def send_API_request(*args, **kwargs):
         print(response.text)
 
 
-def main():
+def main(BASE_URL,NUM_CLIPS,CLIP_LENGTH):
     processes = list()
 
     # start picamera recording
     with picamera.PiCamera() as camera:
-        for filename in camera.record_sequence('/home/pi/Desktop/project/videos/clip%02d.h264' % i for i in range(5)):
+        for filename in camera.record_sequence('/home/pi/Desktop/project/videos/clip%02d.h264' % i for i in range(NUM_CLIPS)):
             print('Recording to %s' % filename)
             # record 0.5 seconds video
-            camera.wait_recording(0.5)
+            camera.wait_recording(CLIP_LENGTH)
             # create process to send request to API gateway
             p1 = Process(
                         target=send_API_request,  
-                        kwargs={'file_h264': filename, 'VIDEO_NAME':filename.split("/")[-1]}
+                        kwargs={'file_h264': filename, 'VIDEO_NAME':filename.split("/")[-1], 'BASE_URL':BASE_URL}
             )
             p1.start()
             processes.append(p1)
@@ -58,4 +59,14 @@ def main():
         proc.join()
 
 if __name__ == '__main__':
-   main()
+
+    parser = argparse.ArgumentParser(description='Capture videos at regular intervals and make API request to get recognition result')
+    parser.add_argument('--BASE_URL', type=str, default="https://w97zrvuvtb.execute-api.us-east-1.amazonaws.com/dev/", help='the base URL of the face recognition service API')
+    parser.add_argument('--NUM_CLIPS', type=str, default=600, help='the number of clips to be captured')
+    parser.add_argument('--CLIP_LENGTH', type=str, default=0.5, help='the length of each clip to be captured in seconds')
+    
+    args = parser.parse_args()
+    BASE_URL = args.BASE_URL
+    NUM_CLIPS = args.NUM_CLIPS
+    CLIP_LENGTH = args.CLIP_LENGTH
+    main(BASE_URL,NUM_CLIPS,CLIP_LENGTH)
